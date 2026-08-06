77c3533ee7f6f62dcc85213fed6a4fe782cfae91
lokesh <lokesh.77664@gmail.com>
2026-08-06 22:40:51 +0530

    router get and post method i

--- mongodb/app.js
+++ mongodb/app.js
@@ -4,9 +4,60 @@ app.use(express.json);
 
 let books = [{
     id: 1,
-    title: book1,
+    title: "book1",
 },
 {
     id:2,
-    title:book2,
+    title:"book2",
 }]
+
+
+app.get(("/", (req,res) =>{
+    res.json({
+        Message : " welcoome "
+    })
+}))
+
+
+
+
+app.get(("/get", (req,res) =>{
+    res.json(books)
+}))
+
+
+app.post("/add", (req,res) => {
+      const newBook = {
+    id: Math.floor(Math.random() * 1000).toString(),
+    title: `Book ${Math.floor(Math.random() * 1000)}`,
+}   
+books.push(newBook);
+res.status(200).json({
+    data : " received",
+    book :  newBook,
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server is now running on port ${PORT}`);
+});
